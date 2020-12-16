295779e715d8056982a50a27a1f3fa990566e96b
Denis <dineresc@gmail.com>
2020-12-16 00:45:26 -0300

    creating routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './routes/current-user';
+import { signoutRouter } from './routes/signout';
+import { signinRouter } from './routes/signin';
+
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentUser', (req, res) => {
-    res.send("hi there");
-    
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
 
 app.listen(3000, () => {
     console.log("listening port 3000...");
