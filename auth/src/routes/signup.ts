bb584a8cba2fb84d2a9fbe5dade50ebd61eb0260
Denis <dineresc@gmail.com>
2020-12-18 11:38:21 -0300

    validators express for signup route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post('/api/users/signup', () => {
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("this email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("password must be between 4 and 20 chars"),
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
 
-});
+    if (!email || typeof email !== "string") {
+      res.status(400).send("Provide a valid email");
+    }
+  }
+);
 
 export { router as signupRouter };
